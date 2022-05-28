b63edf8e4a6cc29c88f547020ed6ece746c8540c
ALONKIGLER <kiglerr@gmail.com>
2022-05-28 19:53:46 +0300

    po

--- static/product.js
+++ static/product.js
@@ -1,53 +1,96 @@
+let cuont;
 
 
+window.onload = function(){
 
-function newToy(){
-  
+    const activePage = window.location.pathname;
+    console.log(activePage);
+    
+     document.querySelectorAll('a').forEach(link => {    
+      if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+      }
+    });
+         
+}
+    
+    
+function isHeInsed(producrId) {
 
-    var prod;
-    prod.addTOHtml2();
+    cuont = 0;
+    const navLinks = document.querySelectorAll('span');
+    navLinks.forEach(bla);
 
-    for (var i = 0; i < json.length; i++) {
-        if (json[i].catagori === "-+4") {
-        prod = new Product(json[i].name , "blabla" ,json[i].pic)
-         prod.addTOHtml() 
-        }
+    function bla(A) {
+      if(A.textContent.includes(producrId)){
+        cuont +=1;
     }
+      }
 
+
+    if (cuont > 0) {
+        return (false)
+    } else {
+        return (true)
+     }
 
 }
 
-function newToy1(){
-    
-    var prod;
-    prod.addTOHtml2();
-    
+function CLEAR() {
+
+const list = document.getElementById("HR");
+while (list.hasChildNodes()) {
+ list.removeChild(list.firstChild);
+
+}}
+
+
+
+function newToy() {
+
+    CLEAR()
+
     for (var i = 0; i < json.length; i++) {
-        if (json[i].catagori === "-+8") {
-        prod = new Product(json[i].name , "blabla" ,json[i].pic)
-        prod.addTOHtml() 
+        if (json[i].catagori === "-+4" && isHeInsed(json[i].name)) {         
+            prod = new Product(json[i].name, "blabsa", json[i].pic)
+            prod.addTOHtml()
         }
     }
 }
 
-function newToy2(){
-    
+function newToy1() {
+    CLEAR()
+
     for (var i = 0; i < json.length; i++) {
-        if (json[i].catagori === "-+9") {
-        var prod = new Product(json[i].name , "blabla" ,json[i].pic)
-         prod.addTOHtml() 
+        if (json[i].catagori === "-+8" && isHeInsed(json[i].name)) {
+            prod = new Product(json[i].name, "blabla", json[i].pic)
+            prod.addTOHtml()
+        }
+    }
+}
+
+function newToy2() {
+
+    CLEAR()
+    for (var i = 0; i < json.length; i++) {
+        if (json[i].catagori === "-+9" && isHeInsed(json[i].name)) {
+            var prod = new Product(json[i].name, "blabla", json[i].pic)
+            prod.addTOHtml()
         }
 
     }
-
 }
 
-function newToy3(){
-    
-    for (var i = 0; i < json.length; i++) {
-        var prod = new Product(json[i].name , "blabla" ,json[i].pic)
-         prod.addTOHtml() 
-        }
+
+function newToy3() {
+    CLEAR()
+   
+    for (var i = 0; i < json.length; i++){
+        if (isHeInsed(json[i].name)) {
+        var prod = new Product(json[i].name, "blabla", json[i].pic)
+        prod.addTOHtml()
+    }
+}
 }
 
 
